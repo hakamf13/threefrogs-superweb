c5ec7dbdb271f5aa1fb9ccd8a24d2e050ff69843
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2026-04-11 17:27:18 +0700

    fix again

--- src/app/admin/stores/[id]/page.tsx
+++ src/app/admin/stores/[id]/page.tsx
@@ -23,7 +23,9 @@ export default async function AdminStoreDetailPage({
 	}
 
 	const { id } = await params;
-// TEST
+// TESTING
+// testing
+
 	const store = await prisma.store.findUnique({
 		where: { id },
 		include: {
